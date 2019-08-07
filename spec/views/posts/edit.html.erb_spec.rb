f78dec464a7ddc9d702bd94306e50daefd8f6b5e
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-07 00:11:02 +0000

    Automatically backed up by Learn

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "posts/edit", :type => :view do
   before(:each) do
     @post = assign(:post, Post.create!(
-      :title => "MyString"
+      :title => "MyString",
+      :description => "MyDescription"
     ))
   end
 
